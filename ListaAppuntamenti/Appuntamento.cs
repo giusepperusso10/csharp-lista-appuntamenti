5f5be1ce88dddcb9fa1af1ba86eab7368ab42c77
Giuseppe Russo <russogiuseppe412@gmail.com>
2022-04-21 15:00:19 +0200

    Create Appuntamento.cs

---
+++ ListaAppuntamenti/Appuntamento.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace ListaAppuntamenti
+{
+	public class Appuntamento
+	{
+		private DateTime dataOra;
+		private string nome { get; }
+		private string localitaAppuntamento { get; }
+
+		public Appuntamento(DateTime dataOra, string nome, string localitaAppuntamento)
+		{
+			this.dataOra = ControlloDataEsatta(dataOra);
+			this.nome = nome;
+			this.localitaAppuntamento = localitaAppuntamento;
+			
+		}
+
+		public DateTime ControlloDataEsatta(DateTime dataOra)
+        {
+			this.dataOra = dataOra;
+			DateTime dataOraAttuale = DateTime.Now;
+
+			if (dataOra >= dataOraAttuale)
+            {
+				Console.WriteLine("Data accettata");
+				return dataOra;
+            } else
+            {
+				Console.WriteLine("Data non accettata");
+			}
+			return dataOra;
+        }
+
+		public DateTime CambioData(DateTime nuovaData)
+        {
+			this.dataOra = ControlloDataEsatta(nuovaData);
+			return dataOra;
+        }
+	}
+}
+
